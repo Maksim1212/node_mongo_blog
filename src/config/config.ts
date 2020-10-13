6d4498303b58ebd37408b046d6564901ca1b8354
maksim1212 <mr.frost201611@gmail.com>
2020-10-13 18:18:21 +0300

    refactor: was removed unusible code
    was removed unusible code

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,4 +7,14 @@ export const serviceConfig: ServiceConfigInterface = {
     },
 };
 
+export const MONGODB_URI = 'mongodb://localhost:27017/';
+export const MONGODB_DB_MAIN = 'blog_db';
+export const MONGO_URI = `${MONGODB_URI}${MONGODB_DB_MAIN}`;
+
+export const connectOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+};
+
 export const sessionSecret: string = process.env.SESSION_SECRET || 'keyboard cat';
