b8d320bb1c37619e67e9bc24b42ea17e6ff3cf48
maksim1212 <mr.frost201611@gmail.com>
2020-10-16 10:50:23 +0300

    feat(.github): was added action
    was added action

--- src/middleware/is_auth.ts
+++ src/middleware/is_auth.ts
@@ -9,25 +9,30 @@ export default async function isAuthJWT(req: Request, res: Response, next: NextF
     const requsetUser = req.body.user_id;
     let tokens: TokensInterface;
     let verify;
+
     try {
         const token = req.body.accessToken;
         verify = jwt.verify(token, serviceConfig.jwt.accessSecret);
         const userId = verify.user;
+
         if (requsetUser !== userId) {
             return res.status(403).json({ message: 'wrong token' });
         }
     } catch (error) {
         if (error.message === 'jwt expired') {
             const userId = verify.user;
+
             if (requsetUser !== userId) {
                 return res.status(403).json({ message: 'wrong token' });
             }
+
             tokens = await getJWTTokens(userId);
             const user = await UserService.findByUserId(userId);
             const { accessToken } = tokens;
             req.body.accessToken = accessToken;
             const token = req.body.accessToken;
             verify = jwt.verify(token, serviceConfig.jwt.accessSecret);
+
             if (!user) {
                 return res.status(401).json({ message: 'user not found' });
             }
@@ -35,9 +40,12 @@ export default async function isAuthJWT(req: Request, res: Response, next: NextF
             return res.status(403).json({ message: 'wrong token' });
         }
     }
+
     const currentTime = Math.floor(Date.now() / 1000);
+
     if (verify.exp > currentTime) {
         return next();
     }
+
     return res.status(200);
 }
